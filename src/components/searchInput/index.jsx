2bc629be3b4c4b6b0bb1914a3fe233f2f7135393
ElnazMalakouti <elnazmalakouti@gmail.com>
2022-09-21 16:21:46 +0430

    feat(searchInput):Add search Input

--- src/components/searchInput/index.jsx
+++ src/components/searchInput/index.jsx
@@ -1,15 +1,36 @@
-import { useState } from "react"
 import "./style.css"
+import axios from 'axios';
+import {useEffect , useState } from "react"
+
 
 const SearchInput = () => {
 
+
+    const [countryState , setCountryState] = useState(null)
+
+  useEffect(()=>{
+    axios.get("https://restcountries.com/v3.1/all")
+    .then((data) => setCountryState(data.data))
+    .catch((err) => console.log(err))
+  },[])
+
+
     const [searchState , setSearchState] = useState("")
+    // const [filteredCountryState , setFilteredCountryState] = useState("")
+    
+        const searchFunction = ()=> {
+            const filter = countryState.filter(item => item.name.common.toLowerCase() === searchState.toLowerCase())
+            if(filter.length !== 0){
+                console.log(filter)
+            }
+        }
 
     return(
         <div className="searchInput-Div">
-            {/* <label>Search Country Name:</label> */}
-            <input className="searchInput" value={searchState} onChange={e => setSearchState(e.target.value)}/>
-            {console.log(searchState)}
+            <input placeholder="Search Country Name..." className="searchInput" value={searchState} onChange={e => {
+                setSearchState(e.target.value)
+                searchFunction()
+                }}/>
         </div>
     )
 }
